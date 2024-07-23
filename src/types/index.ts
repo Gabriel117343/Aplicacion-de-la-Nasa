fe38824e32132ae4c700d4e0eed1cbdffe0d9a4e
Gabriel S. <144576892+Gabriel117343@users.noreply.github.com>
2024-07-22 23:39:44 -0400

    Archivo duplicado

--- src/types/index.ts
+++
@@ -1,9 +0,0 @@
-export type PostImage = {
-  date?: String;
-  explanation?: String;
-  hdurl?: String;
-  media_type?: String;
-  service_version?: String;
-  title?: String;
-  url?: String;
-};
--- src/views/Home/Index.tsx
+++
@@ -1,8 +0,0 @@
-import { Home } from './Home';
-
-export const Index = () => {
-
-  return (
-    
-  )
-}
