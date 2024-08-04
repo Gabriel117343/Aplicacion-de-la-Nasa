83d6d6839d1badb41c8f6976f80a5aa6fad472ed
Gabriel S. <144576892+Gabriel117343@users.noreply.github.com>
2024-08-03 23:44:30 -0400

    arreglo de importación

--- ProyectoNasaExpo/app/[date].js
+++ ProyectoNasaExpo/app/[date].js
@@ -2,7 +2,7 @@ import { View, ScrollView } from "react-native";
 import { useLocalSearchParams } from "expo-router";
 import { Stack } from "expo-router";
 import { Header } from "../src/components/descripcion/Header";
-
+import { Descripcion } from "../src/components/descripcion/Descripcion";
 export default function Detail() {
   // se recupera la fecha de la URL
   const { date } = useLocalSearchParams();
