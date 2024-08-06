275da792113522297bc2676b1d1db1a7666e3f24
Gabriel S. <144576892+Gabriel117343@users.noreply.github.com>
2024-08-05 23:19:33 -0400

    Validación de api Key Disponible

--- ProyectoNasaExpo/src/api/nasaApi.js
+++ ProyectoNasaExpo/src/api/nasaApi.js
@@ -1,8 +1,10 @@
 
 import axios from 'axios';
+import Constants from 'expo-constants';
 import { NASA_API_KEY } from '@env' // importa la variable de entorno API_KEY
 
-console.log(NASA_API_KEY)
+const KEY = NASA_API_KEY || Constants.expoConfig.extra.nasaApiKey;
+
 // Creación de una instancia de axios para acceder a la API de la NASA
 const nasaAPI = axios.create({
   baseURL: 'https://api.nasa.gov/planetary/apod'
@@ -10,9 +12,11 @@ const nasaAPI = axios.create({
 
 // esta función puede o no recibir un parámetro opcional para obtener imágenes de la NASA con filtros
 export default function getAllImages (urlParams) {
-
+  if (!KEY) {
+    return Promise.reject(new Error('Se espera una API Key de la NASA'));
+  }
   // el operador de coalesencia nula o Nullish coalescing operator (??) se utiliza para verificar si el valor de la variable es nulo o indefinido
-  return nasaAPI.get(`?api_key=${NASA_API_KEY}${urlParams ?? ''}`);
+  return nasaAPI.get(`?api_key=${KEY}${urlParams ?? ''}`);
   
 }
 
